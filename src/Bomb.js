c80bfa9181f464cd256fd186916cbeb01643d36b
Nilton <niltonroese@gmail.com>
2022-05-03 20:46:03 +1000

    Done

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,25 @@
 // your Bomb code here!
+import React, {Component} from 'react';
+
+export default class Bomb extends Component {
+    constructor(props){
+        super();
+
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    decrementSecondsLeft(){
+        this.setState({
+            secondsLeft: this.state.secondsLeft - 1
+        })
+    
+    }
+
+    render(){
+        return <div><button onClick={() => this.decrementSecondsLeft()}>Decrement Seconds Left></button>
+        {this.state.secondsLeft <= 0 ? "Boom!" : this.state.secondsLeft + "Seconds left before I go boom"}
+        </div>
+    }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,18 @@
 // your ImageSlider code here!
+import React, {Component} from 'react';
+
+export default class ImageSlider extends Component {
+    constructor(props){
+        super();
+
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+    
+    render(){
+        return <div>
+            {this.state.currentSlideIndex}
+                </div>
+    }
+}
